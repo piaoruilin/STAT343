0aba8c77d8f7508ce21a2f815289ce469f8864c4
Yerin Park <95178362+piaoruilin@users.noreply.github.com>
2024-04-21 18:28:13 +0900

    edit

--- 343chapter1.R
+++ 343chapter1.R
@@ -4,13 +4,13 @@
 pi.hat=400/893
 z.test = (pi.hat-0.5)/sqrt(0.5*(1-0.5)/893)
 z.test
--3.112126
+#-3.112126
 pnorm(z.test)
-0.0009287249
+#0.0009287249
 2*pnorm(z.test) # P-value since two-sided testing
-0.00185745
+#0.00185745
 2*(1-pnorm(-z.test)) # P-value is small. Reject the null hypothesis
-0.00185745
+#0.00185745
 
 # We form a 95% approximate confidence interval
 pi.hat-qnorm(0.975)*sqrt(pi.hat*(1-pi.hat)/893)
@@ -32,4 +32,3 @@ Score.stat = (0.9-0.5)/sqrt(0.5*0.5/10)
 # Likelihood Ratio Test 
 LRT.stat = 2*(9*log(9/5)+1*log(1/5))
 1-pchisq(LRT.stat,df=1)
-                                                         

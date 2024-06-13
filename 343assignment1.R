9ed91a13ff79461500b38ffc697922d4f4926659
Yerin Park <95178362+piaoruilin@users.noreply.github.com>
2024-06-13 15:44:06 +0900

    added qnorm logs

--- 343assignment1.R
+++ 343assignment1.R
@@ -1,5 +1,4 @@
 #Question1
-
 486/1500-qnorm(0.995)*sqrt(486/1500*(1-486/1500)/1500)
 #0.2928744
 486/1500+qnorm(0.995)*sqrt(486/1500*(1-486/1500)/1500)
@@ -23,3 +22,27 @@ no=433/8482
 #-0.04533835
 
 #Question4
+pi.c = (570+433)/(555453+8482) # common success probability.
+z.test2=(yes-no)/sqrt(pi.c*(1-pi.c)*(1/555453+1/8482))
+z.test2 # test statistic
+2*pnorm(z.test2) # p-value
+
+#Question5
+chisq.test(matrix(c(433,570,8049,554883),nrow=2),correct=F)
+#Because p-value is small, we reject the null hypothesis and conclude that the two variables are related
+
+#Question6
+RR = yes/no = (570/555453)/(433/8482)
+RR
+
+#Question7
+#LRT Statistic
+2*(802*log(802/516.833)+53*log(53/338.167)+34*log(34/319.167)+494*log(494/208.833))
+#1206.728
+pvalue = 1-pchisq(1206.728,df=1)
+#p-value is 0. So reject null hypothesis. The two variables are not independent.
+
+#Question8
+odds = 802*494/(34*53)
+log(odds)-qnorm(0.975)*sqrt(1/802+1/53+1/34+1/494) #4.947985
+log(odds)+qnorm(0.975)*sqrt(1/802+1/53+1/34+1/494) #5.837998
